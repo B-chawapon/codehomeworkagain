4c37b98fadeb150c01ec5e8aff2cc6ee3f51cca1
Chawapon <68158123+nutt07@users.noreply.github.com>
2020-08-29 17:59:07 +0700

    add got and got-fail add score

--- scorefootball.cpp
+++ scorefootball.cpp
@@ -1,60 +1,63 @@
 #include<stdio.h>
 #include<string.h>
+#include<math.h>
 int main()
 {
-    int i,j,k;
+    int i,j;
     char team[5][50];
     char text[50];
     int score[4]={0};
-    /*int got[4]=0;
-    int fail[4]=0;*/
+    int got[4]={0};
+    int daisea[4]={0};
     int scoreboard[4][4];
-    for(i=1;i<=4;i++)     // TEAM NAME************
+    int max;
+    for(i=0;i<4;i++)     // TEAM NAME************
     {
         scanf("%s",text);
         strcpy(team[i],text);
     }
 
-    for(i=1;i<=4;i++)
+    for(i=0;i<4;i++)
     {
-        for(j=1;j<=4;j++)
+        for(j=0;j<4;j++)
         {
             scanf("%d",&scoreboard[i][j]);
         }
     }
 
-     for(i=1;i<=4;i++)
+     for(i=0;i<4;i++)
     {
-        for(j=1;j<=4;j++)
+        for(j=i+1;j<4;j++)
         {
-           if(i==j)
-            {
-                score[i]+=0;
-            }
-            else if(scoreboard[i][j]>scoreboard[j][i])
-            {
-                score[i]+=3;
-            
-            }
-            else if(scoreboard[i][j]==scoreboard[j][i])
-            {
-                  score[i]+=1;
-            
-            }
-            else if(scoreboard[i][j]<scoreboard[j][i])
-            {
-                score[i]+=0;
-              
-            }
-            
-            
+           if(scoreboard[i][j]>scoreboard[j][i])
+           {
+               score[i]+=3;
+           }
+           else if(scoreboard[i][j]==scoreboard[j][i])
+           {
+               score[i]+=1;
+               score[j]+=1;
+           }
+           else if(scoreboard[i][j]<scoreboard[j][i])
+           {
+               score[j]+=3;
+           }
         }
     }
-    printf("\n%s %d",team[1],score[1]);
-    printf("\n%s %d",team[2],score[2]);
-    printf("\n%s %d",team[3],score[3]);
-    printf("\n%s %d",team[4],score[4]);
+     for(i=0;i<4;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            got[i]+=scoreboard[i][j];
+            daisea[i]+=scoreboard[i][j]-scoreboard[j][i];
+        }
+    }
+   
     
+    printf("\n%s %d %d %d",team[0],score[0],got[0],daisea[0]);
+    printf("\n%s %d %d %d",team[1],score[1],got[1],daisea[1]);
+    printf("\n%s %d %d %d",team[2],score[2],got[2],daisea[2]);
+    printf("\n%s %d %d %d",team[3],score[3],got[3],daisea[3]);
     
  
     

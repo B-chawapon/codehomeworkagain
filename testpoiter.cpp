51c6bb14f643837769e5b0b7487136a9d5c71303
Chawapon <chawapon2001@gmail.com>
2020-08-31 23:21:15 +0700

    fix bug

--- testpoiter.cpp
+++ testpoiter.cpp
@@ -4,14 +4,11 @@ int i;
 int num[3];
 int x=0;
 int max=0;
-
-int main()
+int a=0;
+int count=0;
+int checkmax(int x)
 {
-    for(i=0;i<3;i++)
-    {
-        scanf("%d",&num[i]);
-    }
-   max=num[0];
+    max=num[0];
   for(i=0;i<=4;i++)
   {
       if(num[i]>=max)
@@ -27,10 +24,50 @@ int main()
       }
       else
       {
-          printf("%d",max);
+          a=i-1;
+          //printf("%d",max);
+          num[a]=max;
           break;
       }
   }
+  return max;
+}
+int main()
+{
+    
+    for(i=0;i<3;i++)
+    {
+        scanf("%d",&num[i]);
+    }
+    int limit =checkmax(max);
+   for(a=0;a<=limit;a++)
+   {
+       if(num[0]!=1 &&num[1]!=1&&num[2]!=1)
+       {    
+        checkmax(max);
+        if(max%2==0)
+        {
+            max=max/2;
+            num[a]=max;
+        }
+        else if(max%2==1)
+        {
+            max=max-1;
+            max=max/2;
+            num[a]=max;
+        }
+        count=count+1;
+       }
+       else
+       {
+           printf("aa %d",count);
+           break;
+       }
+       
+        
+    }
+
+
     
 
     return 0;

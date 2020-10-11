9a4ebc627de4e6f2e41521202fcbda9c445cc4f2
Chawapon <chawapon2001@gmail.com>
2020-10-11 17:37:01 +0700

    fix bug

--- dna.cpp
+++ dna.cpp
@@ -12,23 +12,6 @@ int main()
 {
     scanf("%s",dna1);
     scanf("%s",dna2);
-
-    /*for(i=0;i<=200;i++)
-    {
-        if(dna1[i]!='\0')
-        {
-           printf("%c",dna1[i]);
-        }
-    }
-    printf(" ");
-    for(i=0;i<=200;i++)
-    {
-        if(dna2[i]!='\0')
-        {
-          printf("%c",dna2[i]); 
-        }
-    }
-     printf("\n");*/
     for(i=0;i<=200 && dna1[i]!='\0';i++)
     {
         temp=i;
